366bf3d3cbb83cc1b081ec245186fd2eaab26e56
zhujunyide <31030793+zhujunyide@users.noreply.github.com>
2024-10-19 20:11:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4328,7 +4328,7 @@ var worker_default = {
               }
             });
           default:
-            url.hostname = "www.speedtest.net";
+            url.hostname = "www.speedest.net";
             url.protocol = "https:";
             request = new Request(url, request);
             return await fetch(request);
